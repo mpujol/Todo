a5cb7ba1d8b8a8eedd286309007ab4da3e6bdae7
mpujol <mike.pujol88@gmail.com>
2018-10-01 14:58:52 -0700

    Updated network Code Tests

--- ToDo/APIClient.swift
+++ ToDo/APIClient.swift
@@ -14,19 +14,51 @@ class APIClient {
     
     func loginUser(withName username: String, password: String, completion: @escaping (Token?, Error?) -> Void) {
         
-        let query = "username=\(username)&password=\(password)"
+        
+        let query = "username=\(username.percentEncoded)&password=\(password.percentEncoded)"
         guard let url = URL(string: "https://awesometodos.com/login?\(query)") else { fatalError() }
         session.dataTask(with: url) { (data, response, error) in
-            //
-        }
+            guard error == nil else {
+                return completion(nil, error)
+            }
+            guard let data = data else {
+                completion(nil,WebServiceError.DataEmptyError)
+                return
+            }
+            do {
+                let dict = try JSONSerialization.jsonObject(with: data, options: []) as? [String:String]
+                
+                let token: Token?
+                if let tokenString = dict?["token"] {
+                    token = Token(id: tokenString)
+                } else {
+                    token = nil
+                }
+                completion(token,nil)
+            } catch {
+            }
+        }.resume()
     }
-    
 }
 
 extension URLSession: SessionProtocol {
     
 }
 
+extension String {
+    var percentEncoded: String {
+        let allowedCharacters = CharacterSet(charactersIn: "/%&=?$#+-~@<>|\\*,.()[]{}^!").inverted
+        guard let encoded = self.addingPercentEncoding(withAllowedCharacters: allowedCharacters) else { fatalError() }
+        return encoded
+    }
+    
+}
+
 protocol SessionProtocol {
     func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+enum WebServiceError: Error {
+    case DataEmptyError
+    case ResponseError
 }
--- ToDoTests/APIClientTests.swift
+++ ToDoTests/APIClientTests.swift
@@ -16,7 +16,7 @@ class APIClientTests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         sut = APIClient()
-        mockURLSession = MockURLSession()
+        mockURLSession = MockURLSession(data: nil, urlResponse: nil, error: nil)
         sut.session = mockURLSession
     }
 
@@ -50,14 +50,54 @@ class APIClientTests: XCTestCase {
 
     func test_Login_UsesExptectedQuery() {
         let completion = { (token: Token?, error: Error?) in }
-        sut.loginUser(withName: "dasdom", password: "1234", completion: completion)
+        sut.loginUser(withName: "dasdöm", password: "%&34", completion: completion)
         
         guard let url = mockURLSession.url else {
             XCTFail()
             return
         }
+     
+        XCTAssertEqual(mockURLSession.urlComponents?.percentEncodedQuery, "username=dasd%C3%B6m&password=%25%2634")
+    }
+    
+    func test_Login_WhenSuccesful_CreatesToken() {
+        let jsonData = "{\"token\": \"1234567890\"}".data(using: .utf8)
+        mockURLSession = MockURLSession(data: jsonData,
+                                        urlResponse: nil,
+                                        error: nil)
+        sut.session = mockURLSession
+        let tokenExpectation = expectation(description: "Token")
+        var caughtToken: Token? = nil
+        sut.loginUser(withName: "Foo", password: "Bar") { (token, _) in
+            caughtToken = token
+            tokenExpectation.fulfill()
+        }
         
-        XCTAssertEqual(mockURLSession.urlComponents?.query, "username=dasdom&password=1234")
+        waitForExpectations(timeout: 1) { (_) in
+            XCTAssertEqual(caughtToken?.id, "1234567890")
+        }
+    }
+    
+    func test_Login_WhenJSONIsInvalid_ReturnsError() {
+        
+        let error = NSError(domain: "SomeError", code: 1234, userInfo: nil)
+        
+        let jsonData = "{\"token\": \"1234567890\"}".data(using: .utf8)
+        
+        mockURLSession = MockURLSession(data: jsonData, urlResponse: nil, error: error)
+        
+        sut.session = mockURLSession
+        
+        let errorExpectation = expectation(description: "Error")
+        var catchedError: Error? = nil
+        sut.loginUser(withName: "Foo", password: "Bar") { (token, error) in
+            catchedError = error
+            errorExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { (error) in
+            XCTAssertNotNil(catchedError)
+        }
+        
     }
 
 }
@@ -66,15 +106,49 @@ extension APIClientTests {
     class MockURLSession: SessionProtocol {
         
         var url: URL?
+        private let dataTask: MockTask
+        
         var urlComponents: URLComponents? {
             guard let url = url else { return nil }
             return URLComponents(url: url, resolvingAgainstBaseURL: true)
         }
         
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            dataTask = MockTask(data: data, urlResponse: urlResponse, error: error)
+        }
         
         func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             self.url = url
-            return URLSession.shared.dataTask(with: url)
+            print(url)
+            dataTask.completionHandler = completionHandler
+            return dataTask
+            
+            
         }
     }
+    
+    class MockTask:URLSessionDataTask {
+        private let data: Data?
+        private let urlResponse: URLResponse?
+        private let responseError: Error?
+        
+        typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+        var completionHandler: CompletionHandler?
+        
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            self.data = data
+            self.urlResponse = urlResponse
+            self.responseError = error
+        }
+        
+        override func resume() {
+            DispatchQueue.main.async {
+                self.completionHandler?(self.data,
+                                        self.urlResponse,
+                                        self.responseError)
+            }
+        }
+       
+    }
+    
 }
